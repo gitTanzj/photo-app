0ad29465d16058e5a70bae22f5d91fd9498ed8f7
Tuvi9 <kaspupavel@gmail.com>
2024-06-12 11:32:09 +0300

    Styling for the profile screen

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import './Profile.css';
 import React, { useEffect,useState } from 'react';
 import { useUserContext } from '../hooks/useUserContext';
 import { Post } from './Post';
@@ -14,27 +15,29 @@ export const Profile = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Profile</h1>
-      
-      
-      {state.user.username.length === 0 ? (
-        <div>No profile available</div>
-      ) : ( 
-      
-      <div>
-        <h2>{state.user.username}</h2>
-     <div>
-    
-      {posts.map((post: any) => (
-        <Post key={post._id} title={post.title} description={post.description} images={post.image} author={post.author}/>
-      ))}
-     </div>
+    <div className='profile-container'>
+      <div className='profile'>
+        <div className='user-profile-rect'>
+          <div className="user-profile-circle">
+            <h1>IMG.JPG</h1>
+            <h2>{state.user.username}</h2>
+          </div>
+        </div>
+          {state.user.username.length === 0 ? (
+            <div>No profile available</div>
+          ) : (
+        <div>
+      <div className='user-posts'>
+        {posts.map((post: any) => (
+        <>
+          <Post key={post._id} title={post.title} description={post.description} images={post.image} author={post.author}/>
+          <br/>
+        </>
+        ))}
       </div>
-     
-      
-        
-      )}
+        </div>
+        )}
+      </div>
     </div>
   );
 };
